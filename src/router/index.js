afc9539b9aec6829291954248334df55741c319c
Andrew Truex <andrewtruex@gmail.com>
2022-05-21 09:09:50 -0400

    add redirects

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import HomeView from "../views/HomeView.vue"
 import AboutView from "../views/AboutView.vue"
 
 const router = createRouter({
-  history: createWebHistory("/"),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
@@ -13,11 +13,11 @@ const router = createRouter({
     {
       path: "/about",
       name: "about",
-      // component: AboutView,
+      component: AboutView,
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      // component: () => import("../views/AboutView.vue"),
     },
   ],
 })
